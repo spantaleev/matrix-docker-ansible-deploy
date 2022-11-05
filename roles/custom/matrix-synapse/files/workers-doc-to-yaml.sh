a9e2607d80e612339e143bc59f817ee6e9d3b104
Slavi Pantaleev <slavi@devture.com>
2022-11-05 07:29:47 +0200

    Fix yaml[comments-indentation] in workers config and remove automation

--- roles/custom/matrix-synapse/files/workers-doc-to-yaml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Fetch the synapse worker documentation and extract endpoint URLs
-# matrix-org/synapse master branch points to current stable release
-# and put it between `workers:start` and `workers:end` tokens in ../vars/main.yml
-
-snippet="$(curl -L https://github.com/matrix-org/synapse/raw/master/docs/workers.md | awk -f workers-doc-to-yaml.awk)"
-awk -v snippet="$snippet" -i inplace '/workers:start/{f=1;print;print snippet}/workers:end/{f=0}!f' ../vars/main.yml
